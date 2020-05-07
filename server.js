58241b23f222e63dfe456f2932bbbd1900a3b947
HeyBrunoXavier <brunoxavier5230@gmail.com>
2020-05-07 03:06:03 -0300

    Autenticação do login e integração com banco MYSQL

--- server.js
+++ server.js
@@ -1,21 +1,62 @@
 const express = require ('express');
+var mysql = require('mysql');
+var session = require('express-session');
+var path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 app.use(express.static('public'));
-// Rotas HTTP da Aplicação
-app.get('/',function(req,res){
-    res.sendFile(__dirname + '/view/index.html')
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//---------------DATABASE----------------
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : 'Bruno5230',
+	database : 'nodelogin'
 });
-app.get('/contratos.html',function(req,res){
-        res.sendFile(__dirname + '/view/contratos.html')
+
+// ROTAS HTTP DA APLICAÇÂO MODULO GET VIEW
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/view/login.html')
+});
+app.get('/index',function(req,res){
+        res.sendFile(__dirname + '/view/index.html')
 });
 app.get('/home.html',function(req,res){
     res.sendFile(__dirname + '/view/home.html')
 });
-// Porta em qual está sendo rodada!
+
+// AUTENTICAÇÂO DE LOGIN
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/index');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+// PORT 8080 STARTED SERVICE
 app.listen(8080,function(erro){
     if(erro){
         console.log('Ocorreu um erro!');
     }else{
-        console.log("Already Connection");
+        console.log("ALREADY CONNECTION IN SERVICE");
     }
 })
